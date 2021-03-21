ac94c870fe868958d2ff3ecba98110a78f45d771
jerry-zz <78629841+jerry-zz@users.noreply.github.com>
2021-03-22 00:52:10 +0800

    Update 备课.py

--- 备课.py
+++ 备课.py
@@ -1,26 +1,31 @@
 from tkinter import *
 
 tk = Tk()
-level = 0
-tk.title('Game')
+Ben_IQ = 100
+canvas = Canvas(tk, width=100, height=100, bg='yellow')
+canvas.pack()
+canvas.create_text(50, 50, text=Ben_IQ, font=('Arial', 50))
 
 
-def b():
-    return level == level + 1
+def A():
+    global Ben_IQ
+    Ben_IQ = Ben_IQ + 1
+    canvas.delete('all')
+    canvas.create_text(50, 50, text=Ben_IQ, font=('Arial', 50))
 
 
-def c():
-    return level == level - 1
+def B():
+    global Ben_IQ
+    if Ben_IQ > 100:
+        Ben_IQ = Ben_IQ - 1
+        canvas.delete('all')
+        canvas.create_text(50, 50, text=Ben_IQ, font=('Arial', 50))
+    else:
+        pass
 
 
-def a():
-    print(level)
-
-
-bt1 = Button(tk, text='+1', command=b)
-bt1.pack()
-bt2 = Button(tk, text='-1', command=c)
+bt = Button(tk, text='IQ+1', command=A)
+bt.pack()
+bt2 = Button(tk, text='IQ-1', command=B)
 bt2.pack()
-bt3 = Button(tk, text='打印', command=a)
-bt3.pack()
 tk.mainloop()
