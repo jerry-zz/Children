2aad033dffcf816c8cebb1a14efa8a875d2b913b
jerry-zz <78629841+jerry-zz@users.noreply.github.com>
2021-04-03 19:14:59 +0800

    Update 备课.py

--- 备课.py
+++ 备课.py
@@ -1,9 +1,18 @@
-from tkinter import *
 import time
-def clock():
-    t=time.localtime()
-    t[]
-tk=Tk()
-canvas=Canvas(tk,width=500,height=100)
+from tkinter import *
+
+tk = Tk()
+canvas = Canvas(tk, width=500, height=500)
 canvas.pack()
 tk.mainloop()
+while 1:
+    t = time.localtime()
+    year = t[0]
+    month = t[1]
+    month_day = t[2]
+    hour = t[3]
+    minute = t[4]
+    second = t[5]
+    week_day = t[6] + 1
+    h_m_s = hour, minute, second
+    print(h_m_s)
