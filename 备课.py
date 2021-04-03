8e86e316e2c162f09847ab95eb5da2e3f28179bb
jerry-zz <78629841+jerry-zz@users.noreply.github.com>
2021-04-03 21:11:27 +0800

    Update 备课.py

--- 备课.py
+++ 备课.py
@@ -1,18 +1,2 @@
-import time
 from tkinter import *
-
-tk = Tk()
-canvas = Canvas(tk, width=500, height=500)
-canvas.pack()
-tk.mainloop()
-while 1:
-    t = time.localtime()
-    year = t[0]
-    month = t[1]
-    month_day = t[2]
-    hour = t[3]
-    minute = t[4]
-    second = t[5]
-    week_day = t[6] + 1
-    h_m_s = hour, minute, second
-    print(h_m_s)
+tk=Tk()
