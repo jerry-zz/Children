1b64e778238a84adbaa149c9efe2670fde54791d
jerry-zz <78629841+jerry-zz@users.noreply.github.com>
2021-04-05 07:44:57 +0800

    Update 备课.py

--- 备课.py
+++ 备课.py
@@ -4,6 +4,7 @@ file_name = []
 cat_name = []
 file_list = []
 article_name = []
+map1 = {}
 file_dir = r"C:\Users\jerry\Documents\GitHub\Project\XiaoMingLibrary\Libraries"
 for root, dirs, files in os.walk(file_dir):
     file_name = files
@@ -12,8 +13,10 @@ for root, dirs, files in os.walk(file_dir):
     for a in range(0, len(file_list)):
         cat_name.append(file_list[a][0])
         article_name.append(file_list[a][1])
-    for c in range(0, len(article_name)):
-        article_name[c] = article_name[c].replace(".txt", "")
-    print("file_name:", file_name)
-    print('cat_name:', cat_name)
-    print('article_name:', article_name)
+        article_name[a] = article_name[a].replace(".txt", "")
+        map1['%s' % article_name[a]] = '%s' % cat_name[a]
+cat_name = list(set(cat_name))
+print('file_name:', file_name)
+print('cat_name:', cat_name)
+print('article_name:', article_name)
+print('map1:', map1)
