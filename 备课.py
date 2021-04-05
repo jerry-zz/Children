e53a7df270242fb0e081284f09f0623b794d92c6
jerry-zz <78629841+jerry-zz@users.noreply.github.com>
2021-04-05 09:58:15 +0800

    Update 备课.py

--- 备课.py
+++ 备课.py
@@ -1,5 +1,8 @@
 import os
+from tkinter import *
 
+w = 0
+h = 100
 file_name = []
 cat_name = []
 file_list = []
@@ -15,8 +18,35 @@ for root, dirs, files in os.walk(file_dir):
         article_name.append(file_list[a][1])
         article_name[a] = article_name[a].replace(".txt", "")
         map1['%s' % article_name[a]] = '%s' % cat_name[a]
-cat_name = list(set(cat_name))
-print('file_name:', file_name)
-print('cat_name:', cat_name)
-print('article_name:', article_name)
-print('map1:', map1)
+        cat_name = list(set(cat_name))
+
+
+def all_book():
+    tk.title('全部图书')
+    canvas.delete('all')
+
+
+def start():
+    def all_book1():
+        bt_all_book.place(x=1000, y=1000)
+        all_book()
+
+    tk.title('主页')
+    canvas.create_text(250, 100, text='欢迎来到小明图书馆!', font=('Arial', 40), fill='yellow')
+    bt_all_book = Button(tk, width=20, height=5, text='所有图书', command=all_book1)
+    bt_all_book.pack()
+    bt_all_book.place(x=50, y=300)
+
+
+tk = Tk()
+tk.resizable(0, 0)
+canvas = Canvas(tk, width=500, height=500, bg='black')
+canvas.pack()
+bt_quit = Button(tk, text='退出', fg='red', command=tk.quit)
+bt_quit.pack()
+bt_quit.place(x=450, y=470)
+bt_quit = Button(tk, text='主页', command=start)
+bt_quit.pack()
+bt_quit.place(x=400, y=470)
+start()
+tk.mainloop()
